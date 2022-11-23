c8c82b734c7308437884a3f56bad25e6b1bdf13a
codebyasif/Protfolio <codebyasif@gmail.com>
2022-11-23 20:58:59 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    //===== Progress Bar
+
+    if($('.progress-line').length) {
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width', percent+'%');
+        }, {accY: 0});
+   }
+});
